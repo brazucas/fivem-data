551a74bca900cbf3a443b9a610fc105ca378ef97
Pedro Papadópolis <pplp93@gmail.com>
2021-03-05 18:56:17 -0300

    chore(*): removendo som de fundo

--- resources/[sistema]/fivem/fivem_client.lua
+++ resources/[sistema]/fivem/fivem_client.lua
@@ -1,8 +1,6 @@
-AddEventHandler('onClientMapStart',function()
-	exports.spawnmanager:setAutoSpawn(true)
-	exports.spawnmanager:forceRespawn()
+AddEventHandler('onClientMapStart', function()
+    exports.spawnmanager:setAutoSpawn(true)
+    exports.spawnmanager:forceRespawn()
 end)
 
-Citizen.CreateThread(function()
-	StartAudioScene("CHARACTER_CHANGE_IN_SKY_SCENE")
-end)
+StartAudioScene("CHARACTER_CHANGE_IN_SKY_SCENE")
