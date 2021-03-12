29170ab021276978dbd6def2beccad1ab2e917f5
Pedro Papadópolis <pplp93@gmail.com>
2021-03-12 14:04:33 -0300

    feat(brz_admin): comando /criarveiculo

--- resources/[brz]/[brz_admin]/brz_admin_veiculos/hansolo/hansolo.lua
+++ resources/[brz]/[brz_admin]/brz_admin_veiculos/hansolo/hansolo.lua
@@ -3,11 +3,3 @@ RegisterNUICallback("CriarVeiculo", function(data)
 
     print("[brzAdminVeiculos:criar] " .. tostring(data))
 end)
-
-RegisterCommand("criarveiculo", function(source, args)
-    local user_id = vRP.getUserId(source)
-    local identity = vRP.getUserIdentity(user_id)
-    if vRP.hasPermission(user_id, "administrador.permissao") then
-        TriggerServerEvent('brzNui:mudar-pagina', 'admin/criar-veiculo', nil)
-    end
-end)
--- resources/[brz]/[brz_admin]/brz_admin_veiculos/skywalker.lua
+++ resources/[brz]/[brz_admin]/brz_admin_veiculos/skywalker.lua
@@ -2,3 +2,11 @@ RegisterServerEvent("brzAdminVeiculos:acessar")
 AddEventHandler("brzAdminVeiculos:acessar",function()
     TriggerServerEvent('brzNui:mudar-pagina', 'admin/criar-veiculo', nil)
 end)
+
+RegisterCommand("criarveiculo", function(source, args)
+    local user_id = vRP.getUserId(source)
+    local identity = vRP.getUserIdentity(user_id)
+    if vRP.hasPermission(user_id, "administrador.permissao") then
+        TriggerServerEvent('brzNui:mudar-pagina', 'admin/criar-veiculo', nil)
+    end
+end)
