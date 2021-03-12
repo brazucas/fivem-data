c29db373fae127de0365a0b3ce88ebde7efb5ce8
Pedro Papadópolis <pplp93@gmail.com>
2021-03-12 12:24:39 -0300

    chore(*): criando resource [brz]

---
+++ resources/[brz]/brz_admin/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+game 'gta5'
+
+ui_page "@brz_nui/index.html#/admin/criar-veiculo"
+
+client_script "hansolo/hansolo.lua"
+
+server_script "skywalker.lua"
+
+files {
+    "html/**/*",
+    "html/biomp/*",
+    "html/biomp/**/*",
+    "html/build/*",
+    "html/build/**/*",
+    "html/vendor/*",
+    "html/vendor/**/*",
+    "html/vendor/**/**/*",
+}
--- resources/[brz]/brz_nui/fxmanifest.lua
+++ resources/[brz]/brz_nui/fxmanifest.lua
@@ -1,9 +1,11 @@
 fx_version 'adamant'
 game 'gta5'
 
-ui_page "html/index.html"
+ui_page "nui/index.html"
 
-client_script "browser.lua"
+client_script "hansolo/hansolo.lua"
+
+server_script "skywalker.lua"
 
 files {
     "html/**/*",
