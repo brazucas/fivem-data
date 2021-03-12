f2b3f08896fdd56f540f2361c8a92edebe99210c
Pedro Papadópolis <pplp93@gmail.com>
2021-03-12 14:27:57 -0300

    feat(brz_admin): comando /criarveiculo

--- resources/[brz]/[brz_admin]/brz_admin_veiculos/skywalker.lua
+++ resources/[brz]/[brz_admin]/brz_admin_veiculos/skywalker.lua
@@ -10,7 +10,11 @@ end)
 RegisterCommand("criarveiculo", function(source, args)
     local user_id = vRP.getUserId(source)
     local identity = vRP.getUserIdentity(user_id)
+
+    print("criarveiculo1")
+
     if vRP.hasPermission(user_id, "administrador.permissao") then
+        print("criarveiculo2")
         TriggerServerEvent('brzNui:mudar-pagina', 'admin/criar-veiculo', nil)
     end
 end)
--- resources/[brz]/brz_nui/skywalker.lua
+++ resources/[brz]/brz_nui/skywalker.lua
@@ -1,5 +1,6 @@
 RegisterServerEvent("brzNui:mudar-pagina")
 AddEventHandler("brzNui:mudar-pagina", function(pagina, params)
+    print("brzNui:mudar-pagina Comando recebido " .. pagina)
     local source = source
 
     SendNUIMessage({ event = 'acessar', pagina = pagina, params = params })
