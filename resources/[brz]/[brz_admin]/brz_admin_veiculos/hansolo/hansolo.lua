69a4ee28223ebff15d89dcb7e65e98a35ef3d7c0
Pedro Papadópolis <pplp93@gmail.com>
2021-03-13 13:55:30 -0300

    feat(brz): persistência dos veículos

--- resources/[brz]/[brz_admin]/brz_admin_veiculos/hansolo/hansolo.lua
+++ resources/[brz]/[brz_admin]/brz_admin_veiculos/hansolo/hansolo.lua
@@ -1,5 +1,45 @@
 RegisterNUICallback("CriarVeiculo", function(data, cb)
-    TriggerServerEvent("brzAdminVeiculos:CriarVeiculo", json.encode(data))
+    TriggerServerEvent("brzAdminVeiculos:CriarVeiculo", json.encode(data.data), data.user_id)
 
     cb('ok')
+end)
+
+RegisterNetEvent("BrzSpawnVeiculo")
+AddEventHandler("BrzSpawnVeiculo", function(brzVeiculoId, params)
+    print("debug1 " .. params.modelo)
+    local mhash = GetHashKey(params.modelo)
+    while not HasModelLoaded(mhash) do
+        RequestModel(mhash)
+        Citizen.Wait(10)
+    end
+    print("debug2")
+    if HasModelLoaded(mhash) then
+        print("debug3")
+        local ped = GetPlayerPed()
+        local nveh = CreateVehicle(mhash, GetEntityCoords(ped), GetEntityHeading(ped), true, false)
+        print("debug4")
+        NetworkRegisterEntityAsNetworked(nveh)
+        while not NetworkGetEntityIsNetworked(nveh) do
+            NetworkRegisterEntityAsNetworked(nveh)
+            Citizen.Wait(1)
+        end
+        print("debug5")
+        SetVehicleDoorsLocked(nveh, params.tracado) -- params.trancado
+        SetVehicleEngineOn(nveh, params.motor, true, false) -- params.motor
+        SetVehicleColours(nveh, params.corPrimaria, params.corSecundaria)
+
+        SetVehicleOnGroundProperly(nveh)
+        SetVehicleAsNoLongerNeeded(nveh)
+        SetVehicleIsStolen(nveh, false)
+        SetPedIntoVehicle(ped, nveh, -1)
+        SetVehicleNeedsToBeHotwired(nveh, false)
+        SetEntityInvincible(nveh, false)
+        SetVehicleNumberPlateText(nveh, params.placa) -- params.placa
+        Citizen.InvokeNative(0xAD738C3085FE7E11, nveh, true, true)
+        SetVehicleHasBeenOwnedByPlayer(nveh, params.aVenda)
+        SetVehRadioStation(nveh, "OFF")
+
+        SetModelAsNoLongerNeeded(mhash)
+        print("debug6")
+    end
 end)
--- resources/[brz]/[brz_admin]/brz_admin_veiculos/skywalker.lua
+++ resources/[brz]/[brz_admin]/brz_admin_veiculos/skywalker.lua
@@ -1,5 +1,5 @@
-local Tunnel = module("vrp","lib/Tunnel")
-local Proxy = module("vrp","lib/Proxy")
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 
 RegisterCommand("criarveiculo", function(source, args)
@@ -9,16 +9,64 @@ RegisterCommand("criarveiculo", function(source, args)
     print("criarveiculo1")
 
     if vRP.hasPermission(user_id, "administrador.permissao") then
-        print("criarveiculo2")
+        print("criarveiculo2 " .. user_id)
 
-        TriggerClientEvent("Notify",source,"sucesso","Criando veículo.",8000)
-        TriggerClientEvent("brzNui:MudarPagina", source, "admin/criar-veiculo", { })
+        TriggerClientEvent("Notify", source, "sucesso", "Criando veículo.", 8000)
+        TriggerClientEvent("brzNui:MudarPagina", source, "admin/criar-veiculo", {}, user_id)
     end
 end)
 
 RegisterServerEvent("brzAdminVeiculos:CriarVeiculo")
-AddEventHandler("brzAdminVeiculos:CriarVeiculo",function(paramsStr)
+AddEventHandler("brzAdminVeiculos:CriarVeiculo", function(paramsStr, user_id)
     local params = json.decode(paramsStr)
 
     print("[Criar Veículo] Solicitação recebida: " .. tostring(params))
+
+    if not vRP.hasPermission(user_id, "administrador.permissao") then
+        print("O jogador " .. user_id .. " não possui permissão para criar veículos")
+        return
+    end
+
+    --    {
+    --        "modelo":"adder",
+    --        "corPrimaria":"123123",
+    --        "corSecundaria":"123123",
+    --        "placa":"JIS6612",
+    --        "proprietario":"60496cce047a3a22c089b2a3",
+    --        "trancado":"true",
+    --        "motor":false,
+    --        "transparencia":100,
+    --        "tamanho":"1",
+    --        "valorOriginal":"100",
+    --        "valorVenda":"100",
+    --        "aVenda":true
+    --    }
+
+    local ped = GetPlayerPed(vRP.getUserSource(user_id))
+
+    params["coords"] = GetEntityCoords(ped)
+    params["heading"] = GetEntityHeading(ped)
+
+    local p = promise.new()
+    exports.mongodb:insertOne({
+        collection = "brz_veiculos",
+        document = params
+    }, function(success, result, insertedIds)
+        if success then
+            p:resolve(insertedIds[1])
+        else
+            p:reject("[MongoDB] ERROR " .. tostring(result))
+        end
+    end)
+    local brzVeiculoId = Citizen.Await(p)
+
+    print("vRP.rusers[user_id] " .. user_id)
+
+    local source = vRP.getUserSource(user_id)
+
+    if brzVeiculoId then
+        TriggerClientEvent('BrzSpawnVeiculo', source, brzVeiculoId, params)
+    else
+        TriggerClientEvent("Notify", source, "Oops!", "Um erro ocorreu ao tentar salvar o veículo")
+    end
 end)
